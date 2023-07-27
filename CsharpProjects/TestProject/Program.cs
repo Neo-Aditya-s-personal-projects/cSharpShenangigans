e896871bfbf6fe04f270fcfdf6dd7ba313933663
Aditya Hebbar <Adityarhebbar@gmail.com>
2023-07-27 07:41:28 -0400

    basics, write in Program.cs

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the number:");
+int number = Convert.ToInt32(Console.ReadLine());
+Boolean isPrime = true;
+double counter = 2;
+while (counter < number) {
+    if (number != counter) {
+        int tempInt = Convert.ToInt32(number/counter);
+        if (!(tempInt == number/counter)) {
+            isPrime = false;
+        }
+    }
+}
+
+if (isPrime) {
+    Console.WriteLine("The number is Prime");
+}
+else {
+    Console.WriteLine("The number isn't Prime");
+}
