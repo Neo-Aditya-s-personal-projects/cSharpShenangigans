30f6ab139d769b46930a05f3b732a67ff1b1e207
Aditya Hebbar <Adityarhebbar@gmail.com>
2023-07-27 10:36:42 -0400

    fixed the 1 problem

--- CsharpProjects/TestProject/Math/PrimeChecker.cs
+++ CsharpProjects/TestProject/Math/PrimeChecker.cs
@@ -4,28 +4,35 @@ class PrimeChecker
     {
         Console.WriteLine("Enter the number:");
         double number = Convert.ToInt32(Console.ReadLine());
-        Boolean isComposite = false;
-        double counter = 2;
-        while ((counter < number) && (!isComposite))
+        if (number == 1)
         {
-            if (number != counter)
-            {
-                int tempInt = Convert.ToInt32(number / counter);
-                if (tempInt == number / counter)
-                {
-                    isComposite = true;
-                }
-            }
-            counter++;
-        }
-
-        if (isComposite)
-        {
-            Console.WriteLine("The number is Composite");
+            Console.WriteLine("Neither Prime nor Composite");
         }
         else
         {
-            Console.WriteLine("The number is Prime");
+            Boolean isComposite = false;
+            double counter = 2;
+            while ((counter < number) && (!isComposite))
+            {
+                if (number != counter)
+                {
+                    int tempInt = Convert.ToInt32(number / counter);
+                    if (tempInt == number / counter)
+                    {
+                        isComposite = true;
+                    }
+                }
+                counter++;
+            }
+
+            if (isComposite)
+            {
+                Console.WriteLine("The number is Composite");
+            }
+            else
+            {
+                Console.WriteLine("The number is Prime");
+            }
         }
     }
 }
