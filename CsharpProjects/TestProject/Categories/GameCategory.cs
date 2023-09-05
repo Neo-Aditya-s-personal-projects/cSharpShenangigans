d30117270a152e0b8cd52681a7febe77001fb0ee
Aditya Hebbar <Adityarhebbar@gmail.com>
2023-09-05 10:12:04 -0400

    fixed??

--- CsharpProjects/TestProject/Categories/GameCategory.cs
+++ CsharpProjects/TestProject/Categories/GameCategory.cs
@@ -1,3 +1,16 @@
 class GameCategory {
-    
+    public static void Run()
+    {
+        Console.WriteLine("The subcategories are: \n \n");
+        string[] SubCategories = {"Paper-and-pencil"};
+        for (int i = SubCategories.Length - 1; i >= 0; i--)
+        {
+            Console.WriteLine("** " + SubCategories[i]);
+        }
+        Console.WriteLine("\n \n What subcatgory would you like?");
+        string SubCategory = Console.ReadLine();
+        if (SubCategory == "Paper-and-pencil") {
+            PaperAndPencilGamesSubCategory.Run();
+        }
+    }
 }
